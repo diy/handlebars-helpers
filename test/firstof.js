00dc7c84904019ffd015bdac5e3fba85325aa672
Derek <derekr@me.com>
2013-10-18 14:05:50 -0700

    Fix firstof test

--- test/firstof.js
+++ test/firstof.js
@@ -2,10 +2,13 @@ var test = require('tap').test;
 
 var firstof = require('../lib/firstof');
 
+// Stub out the options object passed to all helpers
+var stub = {};
+
 test('firstof', function (t) {
     t.plan(3);
 
-    t.equals(firstof(null, null, 'test', 'test2'), 'test', 'should return third position "test"');
-    t.equals(firstof('test', null, null, 'test2'), 'test', 'should return first position "test"');
-    t.equals(firstof(null, null, 'test'), 'test', 'should return last position "test"');
+    t.equals(firstof(null, null, 'test', 'test2', stub), 'test', 'should return third position "test"');
+    t.equals(firstof('test', null, null, 'test2', stub), 'test', 'should return first position "test"');
+    t.equals(firstof(null, null, 'test', stub), 'test', 'should return last position "test"');
 });
